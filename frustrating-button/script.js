65ac6b84a2a29100aba796cb205880f6b765d0e5
Adam Zhang <adamorsomething@pop-os.localdomain>
2020-09-13 14:15:42 -0400

    scripting

--- frustrating-button/script.js
+++ frustrating-button/script.js
@@ -6,3 +6,10 @@ document.querySelector("#button").addEventListener("click", e => {
   points++;
   document.querySelector("#points").innerHTML = points;
 });
+
+// button hovered -> move
+document.querySelector("#button").addEventListener("mouseover", e => {
+  let left = Math.floor(Math.random() * 60) + 21;
+  let top = Math.floor(Math.random() * 60) + 21;
+  document.querySelector("#button").style = `left:${left}%;top:${top}%`
+});
