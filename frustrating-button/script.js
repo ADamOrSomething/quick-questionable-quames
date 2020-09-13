3611f3e71e748ee2714e703a2b0078ebfde4048c
Adam Zhang <adamorsomething@pop-os.localdomain>
2020-09-13 14:25:15 -0400

    refactoring

--- frustrating-button/script.js
+++ frustrating-button/script.js
@@ -1,15 +1,21 @@
 // variables
 let points = 0;
 
-// button clicked -> give point
-document.querySelector("#button").addEventListener("click", e => {
+// functions
+givePoint = () => {
   points++;
-  document.querySelector("#points").innerHTML = points;
-});
+  document.querySelector("#points").textContent = points;
+}
 
-// button hovered -> move
-document.querySelector("#button").addEventListener("mouseover", e => {
+moveButton = () => {
   let left = Math.floor(Math.random() * 60) + 21;
   let top = Math.floor(Math.random() * 60) + 21;
   document.querySelector("#button").style = `left:${left}%;top:${top}%`
-});
+}
+
+// event listeners
+// button clicked -> give point, if first point move button
+document.querySelector("#button").onclick = () => { givePoint(); if (points === 1) moveButton(); };
+
+// button hovered -> move only if already have point
+document.querySelector("#button").onmouseover = () => { if (points > 0) moveButton(); }
