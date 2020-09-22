750b8708b4aa5661f6610f120b5b3eb18acc1234
Adam Zhang <adamorsomething@protonmail.com>
2020-09-22 16:10:35 -0400

    ayy getting coord of mouse click works

--- 3d-chess/js/script.js
+++ 3d-chess/js/script.js
@@ -2,8 +2,18 @@ const board = new Board();
 
 const matrix = board.pieceMatrix;
 
-matrix.forEach((e, i) => {
+const render = () => matrix.forEach((e, i) => {
   e.forEach((ele, index) => {
+    // lmao this is some ugly code
     $(`.game-row:nth-child(${8 - i}) .square:nth-child(${index + 1})`).attr('class', `square ${(ele.white ? 'white' : 'black') + '-' + ele.visual}`);
   });
 });
+
+$('.game-wrapper').click(e => {
+  // if you have no idea why this variable is named xmc my friend suggested it
+  const xmc = Math.floor((e.clientX - $('.game-wrapper').offset().left) / $('.square').width());
+  // if you have no idea why this variable is named sprite my other friend suggested it
+  const sprite = 7 - Math.floor((e.clientY - $('.game-wrapper').offset().top) / $('.square').width());
+});
+
+render();
